205c62548e759560edefe03933defd9e56a66234
Ashkan Azadmajd <azadmajd@live.com>
2021-09-25 15:09:22 +0330

    Initial commit

---
+++ src/main/java/ir/dataplatform/springbootjpasampleh2/SpringbootJpaSampleH2Application.java
@@ -0,0 +1,13 @@
+package ir.dataplatform.springbootjpasampleh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaSampleH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaSampleH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ir/dataplatform/springbootjpasampleh2/SpringbootJpaSampleH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.dataplatform.springbootjpasampleh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaSampleH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
